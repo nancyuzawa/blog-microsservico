aea39d31c63189d5dd404384f013dce2322d5a78
Nah~ <110066948+nancyuzawa@users.noreply.github.com>
2026-09-01 16:40:10 -0300

    Update comment fetching URL and content display

--- client/src/CommentList/CommentList.js
+++ client/src/CommentList/CommentList.js
@@ -6,7 +6,8 @@ const CommentList = ({ postId }) => {
 
     const fetchComments = async () => {
         try {
-            const response = await fetch(`http://localhost:5000/posts/${postId}/comments`);
+            // const response = await fetch(`http://localhost:4001/posts/${postId}/comments`);
+            const response = await fetch(`http://localhost:4002/posts/${postId}/comments`);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
@@ -26,14 +27,15 @@ const CommentList = ({ postId }) => {
     const renderedComments = comments.map((comment) => {
         return (
             <li key={comment.id} className="comment-item">
-                {comment.comment}
+                {/* {comment.comment} */}
+                {comment.content}
             </li>
         );
     });
 
     return (
         <div className="container-lista-comentario">
-            <h4 className="comment-count">{comments.length} comentário(s)</h4>
+            <h4 className="comment-count">{comments.length} comment(s)</h4>
             <ul className="comment-list-items">
                 {renderedComments}
             </ul>

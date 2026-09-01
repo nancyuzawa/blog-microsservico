933f71703bc377e79affd267bfeb45e1ac7144f8
Nah~ <110066948+nancyuzawa@users.noreply.github.com>
2026-09-01 16:38:07 -0300

    Make post creation async and add event handling
    Refactor post creation to be asynchronous and handle event posting.

--- Posts/index.js
+++ Posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     if(!req.body || !req.body.title) {
         return res.status(400).send(
             {
@@ -26,7 +26,29 @@ app.post('/posts', (req, res) => {
     const { title } = req.body;
     const newPost = {id, title};
     posts.push(newPost);
+
+    try {
+        const response = await fetch('http://localhost:4005/events', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({type: 'PostCreated', id, title})
+        });
+        if(!response.ok) {
+            throw new Error('Request error on 4005: ', response.statusText)
+        }
+        const data = await response.json();
+        console.log('Response from 4005 ', data);
+    } catch (error) {
+        console.error('Error request. Port 4005', error) // Porta do event-bus
+    }
     res.status(201).send(newPost);
+})
+
+app.post('/events', (req, res) => {
+    console.log('Received event: ', req.body);
+    res.send({});
 })
 
 app.listen(4000, () => {
